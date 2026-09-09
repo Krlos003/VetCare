872e6d965e004995f60936e71e7795bb839f35dd
valentina <valentina.meber@gmail.com>
2026-09-08 23:37:57 -0500

    guardar cambios en atencion medica y menu medico

--- src/main/java/com/vetcare/SistemaMedico/AtencionMedica.java
+++ src/main/java/com/vetcare/SistemaMedico/AtencionMedica.java
@@ -8,69 +8,44 @@ public class AtencionMedica {
     private String descripcion;
     private double costoAtencion;
     private Date fecha;
-    private String nombreMascota;
-    private String diagnostico;
 
-    public AtencionMedica(int idServicio, String descripcion, double costoAtencion, String nombreMascota,
-            String diagnostico) {
-        this.idServicio = idServicio;
-        this.descripcion = descripcion;
-        this.costoAtencion = costoAtencion;
-        this.fecha = new Date();
-        this.nombreMascota = nombreMascota;
-        this.diagnostico = diagnostico;
+public AtencionMedica(int idServicio, String descripcion, double costoAtencion) {
+    this.idServicio = idServicio;
+    this.descripcion = descripcion;
+    this.costoAtencion = costoAtencion;
+    this.fecha = new Date();
     }
 
-    public int getIdServicio() {
-        return idServicio;
+public int getIdServicio() {
+    return idServicio;
     }
 
-    public void setIdServicio(int idServicio) {
-        this.idServicio = idServicio;
+public void setIdServicio(int idServicio) {
+    this.idServicio = idServicio;
     }
 
-    public String getDescripcion() {
-        return descripcion;
+public String getDescripcion() {
+    return descripcion;
     }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
     }
 
-    public double getCostoAtencion() {
-        return costoAtencion;
+public double getCostoAtencion() {
+    return costoAtencion;
     }
 
-    public void setCostoAtencion(double costoAtencion) {
-        this.costoAtencion = costoAtencion;
+public void setCostoAtencion(double costoAtencion) {
+    this.costoAtencion = costoAtencion;
     }
 
-    public Date getFecha() {
-        return fecha;
+public Date getFecha() {
+    return fecha;
     }
+public void setfecha(Date fecha) {
+    this.fecha = fecha;
 
-    public String getNombreMascota() {
-        return nombreMascota;
-    }
+}
 
-    public void setNombreMascota(String nombreMascota) {
-        this.nombreMascota = nombreMascota;
-    }
-
-    public String getDiagnostico() {
-        return diagnostico;
-    }
-
-    public void setDiagnostico(String diagnostico) {
-        this.diagnostico = diagnostico;
-    }
-
-    @Override
-    public String toString() {
-        return "ID: " + idServicio +
-                " | Mascota: " + nombreMascota +
-                " | Servicio: " + descripcion +
-                " | Diagnostico: " + diagnostico +
-                " | Costo: $" + costoAtencion;
-    }
 }
